fafb2b417eb22469b32e034c0656edfa57149f58
Flux45 <aayyuusshh.45@gmail.com>
2023-05-09 18:21:13 +0530

    First Commit

---
+++ src/main/java/com/example/springbootultimate/SpringBootUltimateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootultimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUltimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUltimateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootultimate/SpringBootUltimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootultimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUltimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
